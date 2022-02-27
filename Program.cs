2e57840b30aa5c2eba1e5ffb4a688b4af06104a1
Domenik Milohanić <90246598+XDomeksX@users.noreply.github.com>
2022-02-27 20:19:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljeđivanje
+{
+    class Biljka { }
+
+    class Stablo : Biljka
+    {
+        bool opadajuListovi;
+
+        public Stablo(bool opadajuListovi)
+        {
+            this.opadajuListovi = opadajuListovi;
+        }
+
+        public bool OpadajuListovi { get => opadajuListovi; set => opadajuListovi = value; }
+    }
+
+    class Cvijet : Biljka { }
+
+    class Bijelogoricno : Stablo
+    {
+        public Bijelogoricno(bool opadajuListovi = true) : base(opadajuListovi) { }
+    }
+
+    class Crnogoricno : Stablo
+    {
+        public Crnogoricno(bool opadajuListovi = false) : base(opadajuListovi) { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bijelogoricno hrast = new Bijelogoricno();
+            Crnogoricno bor = new Crnogoricno();
+
+            Console.WriteLine("Hrast : " + hrast.OpadajuListovi);
+            Console.WriteLine("Bor : " + bor.OpadajuListovi);
+
+            Console.ReadKey();
+        }
+    }
+}
